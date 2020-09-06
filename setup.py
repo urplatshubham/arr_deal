c2e2b914a844ef18e12593de581a3432beb80166
Shubham Rajak <39699026+urplatshubham@users.noreply.github.com>
2020-09-06 17:37:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     name="arr_deal",
-    version="0.0.2",
+    version="0.0.1",
     author="Shubham Rajak",
     description="Just a small effort",
     long_description=long_description,
